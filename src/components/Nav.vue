f5782f68e36b40187339e709992e76f993250db3
Tharindu Lakshitha <tharindu.lakshitha.susl@gmail.com>
2020-08-04 14:24:44 +0530

    cleaup

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,7 +1,7 @@
 <template>
     <nav class="navbar navbar-expand-lg navbar-light navbar-laravel app-navigation">
     <div class="container">
-        <router-link to="/" class="navbar-brand">Chat App</router-link>
+        <router-link to="/" class="navbar-brand">SAWI ROW Tailoring System</router-link>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
